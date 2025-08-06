8a625a5f615acf4e3b60075f622f3516db38da5e
KristiyanHr <20118@uktc-bg.com>
2025-08-06 20:42:30 +0300

    Established connection between the Java Spring backend and the React frontend! Finally...

--- src/main/java/com/ecom/productCatalog/controller/CategoryController.java
+++ src/main/java/com/ecom/productCatalog/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecom.productCatalog.controller;
 
 import com.ecom.productCatalog.model.Category;
 import com.ecom.productCatalog.service.CategoryService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     private final CategoryService categoryService;
