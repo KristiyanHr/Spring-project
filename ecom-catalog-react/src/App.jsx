569c8e7fb148e80a391c52a5ffebefebb8390b1c
KristiyanHr <20118@uktc-bg.com>
2025-08-07 14:36:17 +0300

    Finalising the project, added the last functionalities!

--- ecom-catalog-react/src/App.jsx
+++ ecom-catalog-react/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ProductList from './ProductList';
 import CategoryFilter from './CategoryFilter';
@@ -8,77 +6,83 @@ import CategoryFilter from './CategoryFilter';
 function App() {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
 
- 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:8080/api/products')
       .then(response => response.json())
       .then(data => setProducts(data));
 
-      fetch('http://localhost:8080/api/categories')
+    fetch('http://localhost:8080/api/categories')
       .then(response => response.json())
       .then(data => setCategories(data));
-  }, [])
+  }, []);
 
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value)
+    setSearchTerm(event.target.value);
   };
 
   const handleSortChange = (event) => {
-    setSearchTerm(event.target.value)
+    setSortOrder(event.target.value);
   };
 
-  const handleCategorySelect = (event) => {
-    handleCategorySelect(categoryId ? Number(categoryId) : null);
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId ? Number(categoryId) : null);
   };
 
   const filteredProducts = products
-                .filter(product => {
-                  return (
-                    (selectedCategory ? product.category.id === selectedCategory : true)
-                  )
-                });
+              .filter(product => {
+                return (
+            (selectedCategory === null || product.categoryId === selectedCategory)
+            &&
+            product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+              })
+              .sort((a, b) => {
+                if (sortOrder === "asc"){
+                  return a.price - b.price;
+                } else {
+                  return b.price - a.price;
+                }
+              });
 
   return (
     <div className='container'>
-
       <h1 className='my-4'>Product Catalog</h1>
 
       <div className='row align-items-center mb-4'>
         <div className='col-md-3 col-sm-12 mb-2'>
           <CategoryFilter categories={categories} onSelect={handleCategorySelect}/>
         </div>
+
         <div className='col-md-5 col-sm-12 mb-2'>
-          <input 
-          type="text" 
-          className='form-control' 
-          placeholder='Search for products'
-          onChange={handleSearchChange}
-          >
-          </input>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search for products'
+            onChange={handleSearchChange}
+            />
         </div>
-        <div className='cold-md-4 col-sm-12 mb-2'>
+
+        <div className='col-md-4 col-sm-12 mb-2'>
           <select className='form-control' onChange={handleSortChange}>
-            <option value="asc">Sort by price: Low to High</option>
-            <option value="desc">Sort by price: High to Low</option>
+            <option value="asc">Sort by Price: Low to High</option>
+            <option value="desc">Sort by Price: High to Low</option>
           </select>
         </div>
       </div>
 
-
       <div>
-        {products.length ? (
-          <ProductList products={products} />
+        {filteredProducts.length ? (
+          // Display products
+          <ProductList products={filteredProducts}/>
         ) : (
-          <p>No products found</p>
+          <p>No Products Found</p>
         )}
       </div>
     </div>
-
   )
 }
 
