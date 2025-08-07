6e67a54407aad71c95c812f12a273398ffa13b55
KristiyanHr <20118@uktc-bg.com>
2025-08-07 11:55:53 +0300

    The project is coming together

--- ecom-catalog-react/src/App.jsx
+++ ecom-catalog-react/src/App.jsx
@@ -3,18 +3,73 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductList from './ProductList';
+import CategoryFilter from './CategoryFilter';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
  
   useEffect(()=>{
     fetch('http://localhost:8080/api/products')
       .then(response => response.json())
       .then(data => setProducts(data));
+
+      fetch('http://localhost:8080/api/categories')
+      .then(response => response.json())
+      .then(data => setCategories(data));
   }, [])
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  };
+
+  const handleSortChange = (event) => {
+    setSearchTerm(event.target.value)
+  };
+
+  const handleCategorySelect = (event) => {
+    handleCategorySelect(categoryId ? Number(categoryId) : null);
+  };
+
+  const filteredProducts = products
+                .filter(product => {
+                  return (
+                    (selectedCategory ? product.category.id === selectedCategory : true)
+                  )
+                });
 
   return (
     <div className='container'>
+
+      <h1 className='my-4'>Product Catalog</h1>
+
+      <div className='row align-items-center mb-4'>
+        <div className='col-md-3 col-sm-12 mb-2'>
+          <CategoryFilter categories={categories} onSelect={handleCategorySelect}/>
+        </div>
+        <div className='col-md-5 col-sm-12 mb-2'>
+          <input 
+          type="text" 
+          className='form-control' 
+          placeholder='Search for products'
+          onChange={handleSearchChange}
+          >
+          </input>
+        </div>
+        <div className='cold-md-4 col-sm-12 mb-2'>
+          <select className='form-control' onChange={handleSortChange}>
+            <option value="asc">Sort by price: Low to High</option>
+            <option value="desc">Sort by price: High to Low</option>
+          </select>
+        </div>
+      </div>
+
+
       <div>
         {products.length ? (
           <ProductList products={products} />
